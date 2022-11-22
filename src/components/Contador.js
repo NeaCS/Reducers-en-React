3143b941855b497811632094054a376b2b45b4c1
NeaCS <naldi.castro@gmail.com>
2022-11-22 16:24:03 -0300

    intro

--- src/components/Contador.js
+++ src/components/Contador.js
@@ -1,24 +1,32 @@
-import React, { useReducer} from 'react';
+import React, { useReducer } from 'react';
 
-const initialState = {contador:0};
+const initialState = { contador: 0 };
+
+const TYPES = {
+    INCREMENT: "INCREMENT",
+    INCREMENT_5: "INCREMENT_5",
+    DECREMENT: "DECREMENT",
+    DECREMENT_5: "DECREMENT_5",
+    RESET: "RESET"
+}
 
 function reducer(state, action) {
     switch (action.type) {
-        case "INCREMENT":
-        return {contador: state.contador +1 };
-        case "DECREMENT":
-        return {contador: state.contador -1 };
-    
+        case TYPES.INCREMENT:
+            return { contador: state.contador + action.payload };
+        case TYPES.DECREMENT:
+            return { contador: state.contador - action.payload };
+        case TYPES.INCREMENT_5:
+            return { contador: state.contador + action.payload };
+        case TYPES.DECREMENT_5:
+            return { contador: state.contador - action.payload };
+            case TYPES.RESET:
+            return initialState
+
         default:
             return state;
 
     }
-
-
-
-
-    
-     
 
 }
 
@@ -26,23 +34,29 @@ function reducer(state, action) {
 const Contador = () => {
     //const [contador, setcontador] = useState(0);
     const [state, dispatch] = useReducer(reducer, initialState)
-   
-
-   // const sumar = () => setContador(contador+1);
-   const sumar = () => dispatch({type:"INCREMENT"});
-    const restar = () => dispatch({type: "DECREMENT"});
 
 
-  return (
-    <div style={{textAlign:"center"}}>
-        <h2>Contador Reducer</h2>
-        <nav>
-            <button onClick={sumar}>+</button>
-            <button onClick={restar}>-</button>
-        </nav>
-        <h3>{state.contador}</h3>
-    </div>
-  )
+    // const sumar = () => setContador(contador+1);
+    const sumar = () => dispatch({ type: TYPES.INCREMENT, payload: 1 });
+    const restar = () => dispatch({ type: TYPES.DECREMENT, payload: 1});
+    const sumar_5 = () => dispatch({ type: TYPES.INCREMENT_5, payload: 5 });
+    const restar_5 = () => dispatch({ type: TYPES.DECREMENT_5, payload: 5 });
+    const reset = () => dispatch({type: TYPES.RESET })
+
+
+    return (
+        <div style={{ textAlign: "center" }}>
+            <h2>Contador Reducer</h2>
+            <nav>
+                <button onClick={sumar}>+</button>
+                <button onClick={restar}>-</button>
+                <button onClick={reset}>0</button>
+                <button onClick={sumar_5}>+5</button>
+                <button onClick={restar_5}>-5</button>
+            </nav>
+            <h3>{state.contador}</h3>
+        </div>
+    )
 }
 
 export default Contador
